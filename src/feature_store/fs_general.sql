baea24e6a27bcaad54e5e3c114d222b56d28d637
Ryan Rodrigues <ryanrodriguesds@outlook.com>
2024-06-06 03:05:16 -0300

    Feature Store General

---
+++ src/feature_store/fs_general.sql
@@ -0,0 +1,45 @@
+WITH tb_rfv AS (
+
+SELECT 
+    idCustomer,
+    CAST(MIN(julianday('2024-06-04') - julianday(dtTransaction)) AS INTEGER) + 1 AS recenciaDias,
+
+    COUNT(DISTINCT DATE(dtTransaction)) AS frequenciaDias,
+
+    SUM(CASE WHEN pointsTransaction > 0 THEN pointsTransaction END) AS valorPoints
+
+FROM transactions
+
+WHERE dtTransaction < '2024-06-04'
+AND dtTransaction >= DATE('2024-06-04', '-21 days')
+
+GROUP BY idCustomer
+
+),
+
+tb_idade AS (
+
+SELECT
+    t1.idCustomer,
+    CAST(MAX(julianday('2024-06-04') - julianday(dtTransaction)) AS INTEGER) + 1 AS idadeBaseDias
+
+FROM tb_rfv AS t1
+
+LEFT JOIN transactions AS t2
+ON t1.idCustomer = t2.idCustomer
+
+GROUP BY t2.idCustomer
+
+)
+
+SELECT t1.*,
+        t2.idadeBaseDias,
+        t3.flEmail
+
+FROM tb_rfv AS t1
+
+LEFT JOIN tb_idade AS t2
+ON t1.idCustomer = t2.idCustomer
+
+LEFT JOIN customers AS t3
+ON t1.idCustomer = t3.idCustomer
